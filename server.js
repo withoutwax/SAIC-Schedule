adf60b539ebcca0e8ce3e2043371588859a52386
withoutwax <rlagmlckd@gmail.com>
2017-11-19 00:43:15 -0600

    Initialized servers for client and server

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const routes = require('./router/routes');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = 3001;
+
+// Mongoose Connections
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/saicdb',{
+  useMongoClient: true
+});
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(routes);
+
+// Error Handling middleware
+app.use(function(err, req, res, next){
+  console.log('ERROR IS FOUND!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Your SERVER is now running on port ${PORT}. Press CTRL + C to terminate the SERVER`);
+});
